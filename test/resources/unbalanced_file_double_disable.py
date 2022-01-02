c66a2307c18810be0e7e41638cdcab8f10706306
Jack De Winter <jack.de.winter@outlook.com>
2022-01-02 10:33:39 -0800

    Adding testing of scanner.

---
+++ test/resources/unbalanced_file_double_disable.py
@@ -0,0 +1,14 @@
+# pylint: disable=too-many-arguments
+def __print_me(first, second, third, fourth, fifth, sixth, seventh):
+    print(f"first={first}")
+    print(f"second={second}")
+    print(f"third={third}")
+    print(f"fourth={fourth}")
+    print(f"fifth={fifth}")
+    print(f"sixth={sixth}")
+    print(f"seventh={seventh}")
+
+
+# pylint: disable=too-many-arguments
+
+__print_me(1, "2", 3.0, 4, "five", 6.00, "7th")
